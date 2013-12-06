5919856f16eb116b15aaebf1492236a837881958
Todd Lunter <tlunter@gmail.com>
2013-12-06 16:47:46 -0500

    Don't need to use `bundle exec`

--- spec/dockly/cli_spec.rb
+++ spec/dockly/cli_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe "Aerosol CLI" do
+describe "Dockly CLI" do
   describe "running the most basic command" do
-    let(:command) { "bundle exec ./bin/dockly" }
+    let(:command) { "./bin/dockly" }
     it "should exit with 0" do
       expect(system(command)).to be_true
     end
