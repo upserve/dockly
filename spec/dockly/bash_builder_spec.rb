bcffec6783f4cf2ae9598a7ed68c646838454d2b
Todd Lunter <tlunter@gmail.com>
2014-07-25 12:41:51 -0400

    Fix spec

--- spec/dockly/bash_builder_spec.rb
+++ spec/dockly/bash_builder_spec.rb
@@ -74,17 +74,17 @@ describe Dockly::BashBuilder do
     end
   end
 
-  describe "#docker_tag" do
+  describe "#docker_tag_latest" do
     context "when there is no tag" do
       it "does not mark a tag" do
-        output = subject.docker_tag("test_repo")
+        output = subject.docker_tag_latest("test_repo")
         expect(output).to_not include("docker tag")
       end
     end
 
     context "when there is a tag" do
       it "tags the repo:tag as repo:latest" do
-        output = subject.docker_tag("test_repo", "a_tag")
+        output = subject.docker_tag_latest("test_repo", "a_tag")
         expect(output).to include("docker tag test_repo:a_tag test_repo:latest")
       end
     end
