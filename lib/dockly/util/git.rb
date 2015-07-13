b95bbd08d6fe25b1d561864fb8eee64f868b1389
Tom Hulihan <hulihan.tom159@gmail.com>
2015-07-13 12:50:56 -0400

    Remove grit

--- lib/dockly/util/git.rb
+++ lib/dockly/util/git.rb
@@ -1,15 +1,5 @@
-require 'grit'
-
 module Dockly::Util::Git
   module_function
-
-  def git_repo
-    @git_repo ||= Grit::Repo.new('.')
-  end
-
-  def git_sha
-    @git_sha ||= git_repo.git.show.lines.first.chomp.match(/^commit ([a-f0-9]+)$/)[1][0..6] rescue 'unknown'
-  end
 
   def repo
     @repo ||= Rugged::Repository.discover('.')
